b602295415aba22e2eac49c58b395317dc1ec506
pointerrrr <gideonogilvie@gmail.com>
2018-12-13 17:41:10 +0100

    implemented framework of simulated annealing, except neighbor states

--- Groot/Program.cs
+++ Groot/Program.cs
@@ -11,66 +11,29 @@ namespace Groot
     {
         public static void Main(string[] args)
         {
-            string[] afstanden = File.ReadAllLines(@"..\..\afstanden.txt");
+            Tuple<Truck, Truck> trucks = new Tuple<Truck, Truck>(new Truck(0), new Truck(1));
 
-            int maxLenght = int.Parse(afstanden[afstanden.Length - 1].Split(';')[0]);
-            
-            AfstandRijtijd[,] afstandenMatrix = new AfstandRijtijd[maxLenght + 1, maxLenght + 1];
+            LocalSearch search = new LocalSearch();
 
-            for (int i = 1; i < afstanden.Length - 1; i++)
-            {
-                string rij = afstanden[i];
-                string[] info = rij.Split(';');
-                int id1 = int.Parse(info[0]);
-                int id2 = int.Parse(info[1]);
-                int afstand = int.Parse(info[2]);
-                int rijtijd = int.Parse(info[3]);
-                afstandenMatrix[id1, id2] = new AfstandRijtijd(afstand, rijtijd);                
-            }
+            search.FindSolution(trucks);
 
-            
-
-            string[] orderbestand = File.ReadAllLines(@"..\..\orderbestand.txt");
-
-            Dictionary<int, OrderDescription> orders = new Dictionary<int, OrderDescription>();
-
-
-            for(int i = 1; i < orderbestand.Length - 1; i++)
-            {
-                string rij = orderbestand[i];
-
-                string[] info = rij.Split(';');
-                int a = int.Parse(info[0]);
-                string b = info[1];
-                int c = int.Parse(info[2][0].ToString());
-                int d = int.Parse(info[3]);
-                int e = int.Parse(info[4]);
-                float f = float.Parse(info[5]);
-                int g = int.Parse(info[6]);
-                int h = int.Parse(info[7]);
-                int ib = int.Parse(info[8]);
-
-                orders[a] = new OrderDescription(a, b, c, d, e, f, g, h, ib);
-            }
-
-            Truck truck1 = new Truck(1);
-            Truck truck2 = new Truck(2);
-
-            findSolution(truck1, truck2);
-
-            printSolution(truck1, truck2);
+            printSolution(trucks);
 
             Console.ReadLine();
         }
 
-        public static void findSolution(Truck truck1, Truck truck2)
+
+
+        public static void findSolution(Tuple<Truck, Truck> trucks)
         {
 
         }
 
-        public static void printSolution(Truck truck1, Truck truck2)
+        public static void printSolution(Tuple<Truck, Truck> trucks)
         {
-            for(int i = 0; i < 5; i++)
+            Truck truck1 = trucks.Item1;
+            Truck truck2 = trucks.Item2;
+            for (int i = 0; i < 5; i++)
             {
                 for(int j = 0; j < truck1.Dagen[i].Count; j++)
                 {
@@ -98,10 +61,10 @@ namespace Groot
     public struct OrderDescription
     {
         public int Order, Frequentie, AantContainers, VolumePerContainer, MatrixID, XCoordinaat, YCoordinaat;
-        public float LedigingDuurMinuten;
+        public double LedigingDuurMinuten;
         public string Plaats;
 
-        public OrderDescription(int a, string b, int c, int d, int e, float f, int g, int h, int i)
+        public OrderDescription(int a, string b, int c, int d, int e, double f, int g, int h, int i)
         {
             Order = a;
             Plaats = b;
---
+++ Groot/localSearch.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Groot
+{
+    class LocalSearch
+    {
+        int kmax = 100000;
+        double temp = 100d;
+        Random rng = new Random();
+        Tuple<Truck, Truck> bestSolution = new Tuple<Truck, Truck>(new Truck(0), new Truck(1));
+        AfstandRijtijd[,] afstandenMatrix = null;
+        Dictionary<int, OrderDescription> orders = null;
+
+        public LocalSearch()
+        {
+            inladen(afstandenMatrix, orders);
+        }
+
+        public void FindSolution(Tuple<Truck, Truck> trucks)
+        {
+            Tuple<Truck, Truck> currentSolution = new Tuple<Truck, Truck>(new Truck(0), new Truck(1));
+            for (int i = 0; i < kmax; i++)
+            {
+                if(i % 10000 == 0)
+                {
+                    temp *= 0.99d;
+                }
+
+                Tuple<Truck, Truck> randomNeighbor = newNeighbor(currentSolution);
+
+                if (acceptanceChance(currentSolution, randomNeighbor, temp) >= rng.NextDouble() || solutionValue(randomNeighbor) > solutionValue(currentSolution))
+                {
+                    currentSolution = randomNeighbor;
+                    if (solutionValue(currentSolution) > solutionValue(bestSolution))
+                    {
+                        bestSolution = randomNeighbor;
+                    }
+                }
+
+                trucks = bestSolution;
+            }
+        }
+
+        double acceptanceChance(Tuple<Truck,Truck> currentSolution, Tuple<Truck,Truck> randomNeighbor, double temp)
+        {
+            return Math.Exp(((solutionValue(currentSolution) - solutionValue(randomNeighbor) ) / temp ) );
+        }
+
+        // TODO !!!
+        Tuple<Truck, Truck> newNeighbor(Tuple<Truck, Truck> trucks)
+        {
+
+            return null;
+        }
+
+        double solutionValue(Tuple<Truck, Truck> trucks)
+        {
+            double solution = 0;
+            foreach(KeyValuePair<int, OrderDescription> kvp in orders)
+            {
+                OrderDescription order = kvp.Value;
+                if(!orderVoldaan(trucks, order))
+                    solution += order.LedigingDuurMinuten * order.Frequentie * 3;
+            }
+            solution += addTijden(trucks.Item1);
+            solution += addTijden(trucks.Item2);
+            return solution;
+        }
+
+        double addTijden(Truck truck)
+        {
+            double solution = 0;
+            foreach (List<int> dagen in truck.Dagen)
+            {
+                for (int i = 1; i < dagen.Count; i++)
+                {
+                    solution += afstandenMatrix[dagen[i - 1], dagen[i]].Rijtijd;
+                }
+            }
+            return solution;
+        }
+
+        // TODO
+        bool orderVoldaan(Tuple<Truck, Truck> trucks, OrderDescription order)
+        {
+            bool result = true;
+            Predicate<int> p = i => i == order.Order;
+            List<int> a = (List<int>)trucks.Item1.Dagen[0].Concat(trucks.Item2.Dagen[0]);
+            List<int> b = (List<int>)trucks.Item1.Dagen[1].Concat(trucks.Item2.Dagen[1]);
+            List<int> c = (List<int>)trucks.Item1.Dagen[2].Concat(trucks.Item2.Dagen[2]);
+            List<int> d = (List<int>)trucks.Item1.Dagen[3].Concat(trucks.Item2.Dagen[3]);
+            List<int> e = (List<int>)trucks.Item1.Dagen[4].Concat(trucks.Item2.Dagen[4]);
+
+            switch (order.Frequentie)
+            {
+                case 1:
+                    result = a.Exists(p) || b.Exists(p) || c.Exists(p) || d.Exists(p) || e.Exists(p);
+                    break;
+                case 2:
+                    result = (a.Exists(p) && c.Exists(p)) || (b.Exists(p) && d.Exists(p));
+                    break;
+                case 3:
+                    result = a.Exists(p) && b.Exists(p) && c.Exists(p);
+                    break;
+                case 4:
+                    result = (a.Exists(p) ? 1 : 0) + (b.Exists(p) ? 1 : 0) + (c.Exists(p) ? 1 : 0) + (d.Exists(p) ? 1 : 0) + (e.Exists(p) ? 1 : 0) >= 4;
+                    break;
+                case 5:
+                    result = a.Exists(p) && b.Exists(p) && c.Exists(p) && d.Exists(p) && e.Exists(p);
+                    break;
+            }
+            return result;
+        }
+
+        // TODO!!!
+        double temperature(int i, int kmax)
+        {
+            return (double)i / kmax;
+        }
+
+        void inladen(AfstandRijtijd[,] afstandenMatrix, Dictionary<int, OrderDescription> orders)
+        {
+            string[] afstanden = File.ReadAllLines(@"..\..\afstanden.txt");
+
+            int maxLenght = int.Parse(afstanden[afstanden.Length - 1].Split(';')[0]);
+
+            afstandenMatrix = new AfstandRijtijd[maxLenght + 1, maxLenght + 1];
+
+            for (int i = 1; i < afstanden.Length - 1; i++)
+            {
+                string rij = afstanden[i];
+                string[] info = rij.Split(';');
+                int id1 = int.Parse(info[0]);
+                int id2 = int.Parse(info[1]);
+                int afstand = int.Parse(info[2]);
+                int rijtijd = int.Parse(info[3]);
+                afstandenMatrix[id1, id2] = new AfstandRijtijd(afstand, rijtijd);
+            }
+            
+            string[] orderbestand = File.ReadAllLines(@"..\..\orderbestand.txt");
+
+            orders = new Dictionary<int, OrderDescription>();
+
+            for (int i = 1; i < orderbestand.Length - 1; i++)
+            {
+                string rij = orderbestand[i];
+
+                string[] info = rij.Split(';');
+                int a = int.Parse(info[0]);
+                string b = info[1];
+                int c = int.Parse(info[2][0].ToString());
+                int d = int.Parse(info[3]);
+                int e = int.Parse(info[4]);
+                double f = double.Parse(info[5]);
+                int g = int.Parse(info[6]);
+                int h = int.Parse(info[7]);
+                int ib = int.Parse(info[8]);
+
+                orders[a] = new OrderDescription(a, b, c, d, e, f, g, h, ib);
+            }
+        }
+    }
+}
