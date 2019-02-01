d9fda6cff68948840be2a7955a5deb285deff6cb
pointerrrr <gideonogilvie@gmail.com>
2019-02-01 23:35:01 +0100

    changed q

--- Groot/Program.cs
+++ Groot/Program.cs
@@ -12,7 +12,7 @@ namespace Groot
     {
         public static void Main(string[] args)
         {
-            int MaxIterations = 100000000, Q = 100;
+            int MaxIterations = 100000000, Q = 10000;
             double T = 30d;
             try
             {
